7e76a6be60dd5094f53d79b7f0917f18904f578c
knoppmyth <107227128+knoppmyth@users.noreply.github.com>
2025-10-21 01:02:56 -0700

    Modify load_state_dict to include weights_only flag
    For compatibility with PyTorch >= 2.6

--- yolov6/utils/checkpoint.py
+++ yolov6/utils/checkpoint.py
@@ -10,7 +10,7 @@ from yolov6.utils.torch_utils import fuse_model
 
 def load_state_dict(weights, model, map_location=None):
     """Load weights from checkpoint file, only assign weights those layers' name and shape are match."""
-    ckpt = torch.load(weights, map_location=map_location)
+    ckpt = torch.load(weights, map_location=map_location, weights_only=False)
     state_dict = ckpt['model'].float().state_dict()
     model_state_dict = model.state_dict()
     state_dict = {k: v for k, v in state_dict.items() if k in model_state_dict and v.shape == model_state_dict[k].shape}
